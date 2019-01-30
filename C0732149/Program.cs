ec47d0a7f1e786575e9d2d3d5850f78a70776331
visualstudio <visualstudio@122-PC23>
2019-01-30 12:22:18 -0500

    commit

--- C0732149/Program.cs
+++ C0732149/Program.cs
@@ -10,6 +10,14 @@ namespace C0732149
     {
         static void Main(string[] args)
         {
+            int a, b, c, sum;
+            Console.Write("Enter three whole numbers:");
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+            c = int.Parse(Console.ReadLine());
+            sum = a + b + c;
+            Console.Write("The sum is: ={0}", sum);
+            Console.ReadKey();
         }
     }
 }
